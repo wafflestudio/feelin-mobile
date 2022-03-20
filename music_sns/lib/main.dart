886b9dc6dc072015fafdad64614a66342f2ba3bb
kwish <kwish@snu.ac.kr>
2022-03-20 20:12:20 +0900

    Merge branch 'develop' of C:\Users\rlaal\music-sns-mobile with conflicts.

--- music_sns/lib/main.dart
+++ music_sns/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Feelin\'',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -23,6 +23,9 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        primaryColor: const Color.fromARGB(1, 193, 156, 1),
+        fontFamily: 'SpoqaHanSansNeo',
+
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
