7c46531e7e565951964080d981fdf598ab31a9a7
kwish <kwish@snu.ac.kr>
2022-05-10 15:14:35 +0900

    수정

--- music_sns/lib/application/auth/sign_in/sign_in_form/sign_in_form_bloc.dart
+++ music_sns/lib/application/auth/sign_in/sign_in_form/sign_in_form_bloc.dart
@@ -1,9 +1,11 @@
 import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:music_sns/domain/auth/auth_failure.dart';
 import 'package:music_sns/domain/auth/i_auth_repository.dart';
+import 'package:music_sns/domain/auth/token.dart';
 import 'package:music_sns/domain/auth/value_objects.dart';
 
 part 'sign_in_form_event.dart';
@@ -13,6 +15,7 @@ part 'sign_in_form_bloc.freezed.dart';
 @injectable
 class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
   final IAuthRepository _authRepository;
+  final storage = FlutterSecureStorage();
 
   SignInFormBloc(this._authRepository) : super(SignInFormState.initial()) {
     on<_EmailChanged>((event, emit) {
@@ -45,6 +48,10 @@ class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
           ));
         },
       );
+      if(failureOrSuccess.isRight()){
+        storage.write(key: "token", value: ((Right(failureOrSuccess).value) as Token).token);
+        // TODO: navigate To Main page.
+      }
     });
   }
 }
