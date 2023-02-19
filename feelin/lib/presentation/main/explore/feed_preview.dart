13eec14df7e66122ca6359e82e376cc259f1fd75
Hyeonseok Jung <hsjames0248@gmail.com>
2023-02-19 19:01:41 +0900

    feat: Add vendor logo (#31)
    * feat: Add vendor logo
    
    * fix: Remove empty box
    
    * fix: Change image size
    
    * fix: Build g files

--- feelin/lib/presentation/main/explore/feed_preview.dart
+++ feelin/lib/presentation/main/explore/feed_preview.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:music_sns/application/explore/explore_bloc.dart';
+import 'package:music_sns/domain/streaming/vendor.dart';
 import 'package:music_sns/presentation/common/user_nickname.dart';
 import 'package:music_sns/presentation/main/explore/track_preview.dart';
 import 'package:music_sns/presentation/main/playlist_info/playlist_info_page.dart';
@@ -115,10 +116,21 @@ class _FeedPreviewState extends State<FeedPreview> {
                         child: Stack(
                           children: [
                             SizedBox(
-                              width: MediaQuery.of(context).size.width-86,
+                              width: MediaQuery.of(context).size.width - 86,
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
+                                  if(widget.post.playlist.originalVendorPlaylist != null) SizedBox(
+                                    height: 25,
+                                    width: MediaQuery.of(context).size.width,
+                                    child: Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Image.asset(
+                                        Vendor.strToVendor(widget.post.playlist.originalVendorPlaylist!.vendor) == Vendor.spotify ? 'assets/logos/spotify_logo.png' : 'assets/logos/apple_music_logo.png',
+                                        height: 16,
+                                      ),
+                                    ),
+                                  ),
                                   Text(
                                       widget.post.title,
                                     textAlign: TextAlign.start,
